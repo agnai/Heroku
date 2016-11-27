4ca75a3a8e7ac7247146b10202d41fa11a755291
Celvva Vetrivelu <celvaprabu@gmail.com>
2016-11-26 17:26:06 -0800

    test

---
+++ intgserver/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	//	"golang.org/x/oauth2"
+	//	"net/http"
+	"runtime"
+)
+
+/*
+func oathConfig() {
+	conf := &oauth2.Config {
+		ClientID: "Your_Client_Id",
+
+	}
+}
+*/
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println(runtime.NumCPU())
+}
